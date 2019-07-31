c34b72598a653ea45a7ff2904874ac15cff2e137
ipetrovanton <ipetrovanton@gmail.com>
2019-07-31 15:26:19 +0300

    my_chat.sh

---
+++ my_chat.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#func: read file to str; show str and del file
+#called by trap
+my_func(){
+	s=`cat message.txt`
+	echo message from $PID -> $s
+	rm message.txt
+}
+
+#trap used for react on signal
+trap "my_func" SIGUSR1
+
+#show own PID and request for PID of destination point
+echo My PID is $$
+read -p 'Input PID of my partner: ' PID
+
+#main loop: request for text for letter
+#			write it in message.txt file
+#			request action by SIGUSR1 for some process by PID
+while true
+do  
+    echo Write some letter for $PID
+	read message
+	echo $message >> message.txt
+	kill -SIGUSR1 $PID
+done
